4eb28a4a14452d28dedb2843c314867501d11ec5
Adam Robson <adamrayrobson@gmail.com>
2024-12-07 10:42:38 -0800

    add tetrix and projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,14 +6,16 @@ import "../colors.css";
 export default function Page() {
     return (
         <div className="bg-image project-page-container">
+                <h1 className="title">Projects</h1>
+
                 <h1 className="projects-page-title">Projects</h1>
 
                 <ul className="projects-page-list">
-                    <li><Link className="projects-page-list-item" href="#" />project one</li>
-                    <li><Link className="projects-page-list-item" href="#" />project one</li>
-                    <li><Link className="projects-page-list-item" href="#" />project one</li>
-                    <li><Link className="projects-page-list-item" href="#" />project one</li>
-                    <li><Link className="projects-page-list-item" href="#" />project one</li>
+                    <li className="projects-page-list-item"><Link className="projects-page-link-item" href="#" />project one</li>
+                    <li className="projects-page-list-item"><Link className="projects-page-link-item" href="#" />project one</li>
+                    <li className="projects-page-list-item"><Link className="projects-page-link-item" href="#" />project one</li>
+                    <li className="projects-page-list-item"><Link className="projects-page-link-item" href="#" />project one</li>
+                    <li className="projects-page-list-item"><Link className="projects-page-link-item" href="#" />project one</li>
                 </ul>
         </div>
     );
